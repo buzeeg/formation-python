4adcc59535ff8793ad3fe7159be7eaa47b2cf94c
buzeeg <buzeeg@gmail.com>
2021-09-14 12:01:03 +0200

    Test using unittest module

--- test/RationalTest.py
+++ test/RationalTest.py
@@ -4,9 +4,22 @@ from Rational import Rational
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        Rational.simplify()
-        self.assertEqual(True, False)  # add assertion here
+    def test_add(self):
+        r1: Rational = Rational(1, 3)
+        r2: Rational = Rational(2, 1)
+        res: Rational = r1 + r2
+        self.assertEqual(7, res.n)
+        self.assertEqual(3, res.d)
+
+    def test_simplify(self):
+        r = Rational(35, 10)
+        r.simplify()
+        self.assertEqual(7, r.n)
+        self.assertEqual(2, r.d)
+
+    def test_dead(self):
+        # raise Exception("erreur")
+        pass
 
 
 if __name__ == '__main__':
